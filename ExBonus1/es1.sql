4142618d0103c823c507a2a95e3a4cbe37ebd62c
Federico <160421165+Fede046@users.noreply.github.com>
2025-10-26 16:42:07 +0100

    Add support files and update SQL schema for bonus exercise
    Added support files containing sample data for courses, departments, teachings, and students. Updated es1.sql to define new tables (STUDENTE, CORSOLAUREA) and insert sample data. Began definition of DIPARTIMENTOO table. These changes provide a foundation for further development and testing of the bonus exercise.

--- ExBonus1/es1.sql
+++ ExBonus1/es1.sql
@@ -8,11 +8,43 @@ CREATE DATABASE PIANI_STUDIO;
 
 USE PIANI_STUDIO;
 
-CREATE TABLE STUDENTI (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    NOME VARCHAR(100) NOT NULL,
-    COGNOME VARCHAR(100) NOT NULL,
-    EMAIL VARCHAR(255)
+--Stuenti
+CREATE TABLE STUDENTE (
+    Matricola INT PRIMARY KEY,
+    CodiceCL INT NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
+    Cognome VARCHAR(100) NOT NULL,
+    CFUConseguiti INT NOT NULL
 );
 
-SELECT * FROM STUDENTI;
+INSERT INTO STUDENTE (Matricola, CodiceCL, Nome, Cognome, CFUConseguiti) VALUES
+(1,8014,'Luca','Bianchi', 12),
+(2,8014,'Maria','Rossi', 18),
+(3,8014,'Giulia','Neri', 0),
+(4,8014,'Paolo','Verdi', 24),
+(5,8014,'Chiara','Marrone', 6);
+
+
+SELECT * FROM STUDENTE;
+
+--CORSOLAUREA
+
+CREATE TABLE CORSOLAUREA(
+    Codice INT PRIMARY KEY,
+    Nome VARCHAR(100) NOT NULL,
+    NomeDip VARCHAR(100) NOT NULL,
+    NumeroProgrammato VARCHAR(100) NOT NULL
+);
+
+INSERT INTO CORSOLAUREA(Codice, Nome, NomeDip, NumeroProgrammato) VALUES
+(8019,'Informatica','DISI','Nonattivo'),
+(8014,'InfoMan','DISI','Nonattivo'),
+(6061, 'Matematica','MAT','Attivo'),
+(6610,'Economia Aziendale','DISA','Attivo');
+
+SELECT * FROM corsolaurea;
+
+--DIPARTIMENTO
+CREATE Table DIPARTIMENTOO(
+
+);
